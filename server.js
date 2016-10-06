b0df7187f65740ff0fede4977b69401bfc08b466
Tomas Matejka <mtk.eworkers@gmail.com>
2016-10-06 21:52:20 +0200

    ¨Minimum¨

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+var PORT = process.env.PORT || 1337
+var app = express();
+
+if (process.env.NODE_ENV !== 'production') {
+    var webpackDevMiddleware = require('webpack-dev-middleware');
+    var webpackHotMiddleware = require('webpack-hot-middleware');
+    var webpack = require('webpack');
+    var config = require('./webpack.config');
+    var compiler = webpack(config);
+
+    app.use(webpackDevMiddleware(compiler, {
+        noInfo: true,
+        publicPath: config.output.publicPath
+    }));
+    app.use(webpackHotMiddleware(compiler));
+}
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/dist/index.html')
+});
+
+app.listen(PORT, function(error) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.info("==>  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    }
+});
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../assets/stylesheets/base.scss';
+import React, { Component } from 'react';
+
+const Hello = React.createClass({
+  render() {
+    return(
+      <div>
+        Sachy ...
+      </div>
+    )
+  }
+});
+
+export default Hello;
